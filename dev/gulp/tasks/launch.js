84f97d2da9fd18d82917db95bda1e590148a19fa
Anandu B Ajith <reach2anandu@gmail.com>
2015-05-10 21:20:19 +0530

    Fixes wrong Chrome binary location
    using os.platform() to determine the OS , and set the binary location.
    need to find a fix for windows.

--- dev/gulp/tasks/launch.js
+++ dev/gulp/tasks/launch.js
@@ -3,11 +3,18 @@
  **/
 
 var gulp = require('gulp');
+var os = require('os');
 var config = require('../config');
 var utils = require('../utils');
 var plugins = require('gulp-load-plugins')();
+var chrome;
+
+if(os.platform() === "linux"){
+	chrome = "/usr/bin/google-chrome"
+}else if(os.platform() === "darwin"){
+	chrome = "/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome"
+}
 
 gulp.task('launch', ['build:dev-app'] , plugins.shell.task([
-  '/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome'
-  + ' --load-and-launch-app=' + utils.escapeShellArg(config.paths.app)
+  chrome + ' --load-and-launch-app=' + utils.escapeShellArg(config.paths.app)
 ]));
