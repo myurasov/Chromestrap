8c9a3141d8fd50b1b05ceeb02742c57111278333
Mikhail Yurasov <me@yurasov.me>
2015-07-11 04:02:34 -0700

    Prevent watch stopping on incorrect SASS

--- dev/gulp/tasks/compile-styles.js
+++ dev/gulp/tasks/compile-styles.js
@@ -11,6 +11,10 @@ gulp.task('compile-styles', function () {
   return gulp.src(config.paths.app + '/sass/main.scss')
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.sass())
+    .on('error', function (e) {
+      console.log("Failed to compile SASS: ", e.message);
+      this.emit('end');
+    })
     .pipe(plugins.sourcemaps.write('.' /* write as a separate file */))
     .pipe(gulp.dest(config.paths.app_build))
     .pipe(browserSync.reload({stream: true}));
